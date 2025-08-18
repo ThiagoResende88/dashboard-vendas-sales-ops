b455635b95f1f7f3ee851b272beab1aa74a0affe
THIAGO DIAS RESENDE <117482959+ThiagoResende88@users.noreply.github.com>
2025-08-18 16:19:48 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.title').forEach(item => {
-    item.addEventListener('click', event => {
-        const content = item.nextElementSibling;
-        content.classList.toggle('active');
-    });
-});
